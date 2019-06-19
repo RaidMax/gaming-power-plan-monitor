5c18d7d77d3b1331cf976ac7e792fe04c14a8a09
RaidMax <raidmax@live.com>
2019-06-18 19:45:37 -0500

    update to version 0.3 supports ignore/excluded list of images

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 #include <Psapi.h>
 #include <algorithm>
 #include <cctype>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 static bool b_running;
+static bool b_laststate = true;
 const static int i_sleeptime = 10000;
+std::vector<std::string> v_ignoredimages;
 const static std::string lpszRenderLibraries[2] =
 {
 	"libEGL",
@@ -23,6 +28,7 @@ BOOL WINAPI HandlerRoutine(_In_ DWORD dwCtrlType)
 
 bool IsMatchCaseInsensitive(std::string str1, std::string str2)
 {
+	// we're just converting the characters to uppercase so we can do a simple comparison
 	std::transform(str1.begin(), str1.end(), str1.begin(), [](unsigned char c) { return std::toupper(c); });
 	std::transform(str2.begin(), str2.end(), str2.begin(), [](unsigned char c) { return std::toupper(c); });
 
@@ -41,6 +47,7 @@ bool IsRenderLibraryLoaded(DWORD dwProcessId)
 
 		if (EnumProcessModulesEx(hProcess, lphLoadedModules, sizeof(lphLoadedModules), &dwModuleBytes, LIST_MODULES_ALL))
 		{
+			// calculates the number of modules returned
 			dwModuleCount = dwModuleBytes / sizeof(HMODULE);
 			TCHAR szModuleName[128];
 
@@ -48,12 +55,37 @@ bool IsRenderLibraryLoaded(DWORD dwProcessId)
 			{
 				if (GetModuleBaseNameA(hProcess, lphLoadedModules[i], szModuleName, sizeof(szModuleName)))
 				{
+					// calculates how many libraries to search for
 					int iRenderLibraryCount = sizeof(lpszRenderLibraries) / sizeof(std::string);
 
 					for (int j = 0; j < iRenderLibraryCount; j++)
 					{
 						if (IsMatchCaseInsensitive(std::string(szModuleName), std::string(lpszRenderLibraries[j])))
 						{
+							DWORD dwCharWrittenCount = MAX_PATH;
+							char szNameBuffer[MAX_PATH];
+							
+							if (QueryFullProcessImageNameA(hProcess, 0, szNameBuffer, &dwCharWrittenCount)  && dwCharWrittenCount > 0)
+							{
+								for (std::string image : v_ignoredimages)
+								{
+									if (IsMatchCaseInsensitive(szNameBuffer, image))
+									{
+										return false;
+									}
+								}
+
+								if (!b_laststate)
+								{
+									std::cout << "found " << szModuleName << " in " << szNameBuffer << std::endl;
+								}
+							}
+
+							else
+							{
+								std::cout << "encountered error code "  << GetLastError() << " while getting process name for pid " << dwProcessId << std::endl;
+							}
+
 							return true;
 						}
 					}
@@ -78,9 +110,15 @@ int main(char* argc, char** argv)
 	DWORD dwProcessIdsCount;
 	DWORD dwProcessIdsBytes;
 	HINSTANCE hShellResult;
-	bool bLastState = true;
 
-	std::cout << "gaming power plan monitor v0.2\n";
+	std::cout << "gaming power plan monitor v0.3\n";
+
+	std::ifstream hIgnore("ignore.cfg");
+	
+	for (std::string szTmp; std::getline(hIgnore, szTmp);)
+	{
+		v_ignoredimages.push_back(szTmp);
+	}
 
 	do
 	{
@@ -95,16 +133,20 @@ int main(char* argc, char** argv)
 				bRenderLibraryLoaded |= IsRenderLibraryLoaded(lpdwProcessIds[i]);
 			}
 
-			if (bLastState != bRenderLibraryLoaded || !b_running)
+			// b_running is false the first time the application runs the loop, so it should force a powerstate update
+			if (b_laststate != bRenderLibraryLoaded || !b_running)
 			{
 				bRenderLibraryLoaded ?
 					std::cout << "there appears to be a rendering library loaded, switching to max performance...\n" :
 					std::cout << "there does not appear to be a rendering library loaded, switching to balanced...\n";
-				bLastState = bRenderLibraryLoaded;
 
-				if ((hShellResult = (bLastState ? ShellExecuteA(GetDesktopWindow(), "open", "cmd.exe", "/C powercfg /S SCHEME_MIN", 0, 0) : ShellExecuteA(GetDesktopWindow(), "open", "cmd.exe", "/C powercfg /S SCHEME_BALANCED", 0, 0))) < (HINSTANCE)31)
+				b_laststate = bRenderLibraryLoaded;
+
+				if ((hShellResult = (b_laststate ?
+					ShellExecuteA(GetDesktopWindow(), "open", "cmd.exe", "/C powercfg /S SCHEME_MIN", 0, 0) :
+					ShellExecuteA(GetDesktopWindow(), "open", "cmd.exe", "/C powercfg /S SCHEME_BALANCED", 0, 0))) < (HINSTANCE)31)
 				{
-					std::cout << "encountered hinstance error code " << hShellResult << " while attempting to change power plan\n";
+					std::cout << "encountered error code " << hShellResult << " while attempting to change power plan\n";
 				};
 			}
 		}
